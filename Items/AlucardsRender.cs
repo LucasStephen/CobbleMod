4c0e2705efb01533ba1922e24acc81a2e76ee7d2
Unknown <blazor18@jcu.edu>
2018-06-12 22:41:07 -0400

    reaper3shot

--- Items/AlucardsRender.cs
+++ Items/AlucardsRender.cs
@@ -1,3 +1,7 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -28,6 +32,18 @@ namespace CobbleMod.Items
 			item.shoot = mod.ProjectileType("RedKnifeProjectile");
 			item.shootSpeed = 10f;
 		}
+		public override bool Shoot(Player player, ref Microsoft.Xna.Framework.Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+			float numberProjectiles = 3; // 3 shots
+            float rotation = MathHelper.ToRadians(45);//Shoots them in a 45 degree radius. (This is technically 90 degrees because it's 45 degrees up from your cursor and 45 degrees down)
+            position += Vector2.Normalize(new Vector2(speedX, speedY)) * 45f; //45 should equal whatever number you had on the previous line
+            for (int i = 0; i < numberProjectiles; i++)
+            {
+                Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1))) * .2f; // Vector for spread. Watch out for dividing by 0 if there is only 1 projectile.
+                Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI); //Creates a new projectile with our new vector for spread.
+            }
+            return false; //makes sure it doesn't shoot the projectile again after this
+        }
 
 		public override void AddRecipes()
 		{
