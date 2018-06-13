75b3dfe20607d447ae275d4ca1ae7d04550eb2db
Stephen Lucas <sjl3437@rit.edu>
2018-06-12 23:58:09 -0400

    updates to damage and pngs

--- Items/DaddyNlsBrimstone.cs
+++ Items/DaddyNlsBrimstone.cs
@@ -7,31 +7,32 @@ using Terraria.ModLoader;
 
 namespace CobbleMod.Items
 {
-	public class DaddyNlsBrimstone : ModItem
-	{
-		public override void SetStaticDefaults()
-		{
-			DisplayName.SetDefault("Daddy NL's Brimstone");
-			Tooltip.SetDefault("TOMO WHOS MY SPECIAL BOY?");
-		}
-		public override void SetDefaults()
-		{
-			item.noMelee = true;
-			item.noUseGraphic = false;
-			item.magic = true;
-			item.channel = true;
-			item.mana = 0;
-			item.rare = 5;
-			item.width = 28;
-			item.height = 30;
-			//item.usetime = 7;
-			item.UseSound = SoundID.Item13;
-			item.useStyle = 5;
-			item.shootSpeed = 4f;
-			item.useAnimation = 7;
-			item.shoot = mod.ProjectileType("DaddyNlsBrimstoneProjectile");
-			item.value = Item.sellPrice(0,3,0,0);
-		
-		}
-	}
+    public class DaddyNlsBrimstone : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Daddy NL's Brimstone");
+            Tooltip.SetDefault("TOMO WHOS MY SPECIAL BOY?");
+        }
+        public override void SetDefaults()
+        {
+            item.damage = 500;
+            item.noMelee = true;
+            item.noUseGraphic = false;
+            item.magic = true;
+            item.channel = true;
+            item.mana = 0;
+            item.rare = 5;
+            item.width = 28;
+            item.height = 30;
+            //item.usetime = 7;
+            item.UseSound = SoundID.Item13;
+            item.useStyle = 5;
+            item.shootSpeed = 4f;
+            item.useAnimation = 7;
+            item.shoot = mod.ProjectileType("DaddyNlsBrimstoneProjectile");
+            item.value = Item.sellPrice(0, 3, 0, 0);
+        }
+
+    }
 }
